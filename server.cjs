ab796d43bb51890f66be4e343c72417d3cb797ad
EkeneOzoalor <88558745+EkeneOzoalor@users.noreply.github.com>
2022-01-29 04:28:39 -0800

    fix

--- server.cjs
+++ server.cjs
@@ -6,10 +6,10 @@ const knex = require('knex');
   const db=  knex({
     client: 'pg',
     connection: {
-        connectionString: 'process.env.DATABASE_URL',
+        connectionString: process.env.DATABASE_URL,
         ssl: true
-    }
-  });
+     }
+     });
 
  db.select('*').from('users').then(data => {
     console.log(data);
